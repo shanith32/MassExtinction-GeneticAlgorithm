bae48a9b4b1772dae75ca353f123adbadcc25523
Nadika Bandara <nsbandara32@gmail.com>
2018-12-09 00:53:57 -0600

    Chnage in greedy algo

--- greedy.py
+++ greedy.py
@@ -1,19 +1,20 @@
-#Heuristic algorithm to deal with the Knapsack problem
+# Heuristic algorithm to deal with the Knapsack problem
 
 data = [[4, 2, 0], [5, 2, 1], [7, 11, 2],
         [7, 11, 3]]  # object set with the properties - [value, weight, id]
 
-container = []  #the container to contain the objects in
-capacity = 10  #weight capacity of the container
-solution = [0] * len(data)  #binary representation of the soultion
+container = []  # the container to contain the objects in
+capacity = 10  # weight capacity of the container
+solution = [0] * len(data)  # binary representation of the soultion
 
-ratios = []  #an array to store the ratios of each object
-count = 0  #a count to track the weights of objects before adding them to the container
+ratios = []  # an array to store the ratios of each object
+count = 0  # a count to track the weights of objects before adding them to the container
 
 # For loop to get the ratios of value/weight and add the ratios of each object to the container
 for i in range(len(data)):
     ratio = data[i][0] / data[i][1]
     ratios.append([ratio, data[i][2]])
+
 print(data)
 print(ratios)
 # Sort the resulting ratios in decending order
