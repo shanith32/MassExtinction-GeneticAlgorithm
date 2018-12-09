14c23b626569f7ed45d26158e8bf9f861a5ad36b
Nadika Bandara <nsbandara32@gmail.com>
2018-12-09 00:53:26 -0600

    Greedy Algo + crossover, selection and mutation functions

--- functions.py
+++ functions.py
@@ -14,17 +14,37 @@ def main():
 
     initailPopulation = generateInitialPopulation(popSize, capacity, data)
     print("The Randomly Generated INITIAL POPULATION is ", initailPopulation)
+    # parent selection
+    parentOne = selection(initailPopulation)
+    parentTwo = selection(initailPopulation)
+    print("parentOne: ", parentOne, " parentTwo: ", parentTwo)
+    # crossover
+    offSpringSolution = crossover(data, parentOne, parentTwo)
+    print("OffSpringSolution after crossover: ", offSpringSolution)
+    mutatedOffSpring = mutation(data, offSpringSolution, probability)
+    print("OffSpringSolution after mutation: ", mutatedOffSpring)
+    newOffSpringChomosome = encodeChromosome(data, mutatedOffSpring)
+    print("New OffSpring Chromosome: ", newOffSpringChomosome)
+    # genCount = 0
+    # while (genCount)
 
 
-def generateChromosome(data):
-    chromosome = [] * 3  # [[binary soultion], fitness, total weight]
+# Functions
+# Generate a random solution
+def generateRandomSolution(data):
     solution = []
-
     for i in range(len(data)):
         select = random.randint(0, 1)
         solution.append(select)
+    return solution
+
+
+# Encode a chromosome
+def encodeChromosome(data, solution):
+    chromosome = [] * 3  # [[binary soultion], fitness, total weight]
     fitness = 0
     totalWeight = 0
+
     for k in range(len(solution)):
         if (solution[k] == 1):
             fitness += data[k][0]
@@ -35,16 +55,57 @@ def generateChromosome(data):
     return chromosome
 
 
+# Generate a population of random chromosomes
 def generateInitialPopulation(popSize, capacity, data):
     # Initial Population
     initailPopulation = []  # The inital population array
     initCount = 0
     while (initCount != popSize):
-        newChromosome = generateChromosome(data)
+        randSolution = generateRandomSolution(data)
+        newChromosome = encodeChromosome(data, randSolution)
         if (newChromosome[2] <= capacity):
             initailPopulation.append(newChromosome)
             initCount += 1
     return initailPopulation
+
+
+# Two Tournament Selection
+def selection(population):
+    chromoOne = random.choice(population)
+    chromoTwo = random.choice(population)
+
+    if(chromoOne[1] > chromoTwo[1]):
+        return chromoOne
+    else:
+        return chromoTwo
+
+
+# One point Crossover
+def crossover(data, parentOne, parentTwo):
+    cutPoint = len(parentOne[0])//2
+    done = 0
+    offSpringSolution = []
+    while(done < cutPoint):
+        offSpringSolution.append(parentOne[0][done])
+        done += 1
+    while(done < len(parentOne[0])):
+        offSpringSolution.append(parentTwo[0][done])
+        done += 1
+
+    return offSpringSolution
+
+
+# One index flip Mutation
+def mutation(data, solution, probability):
+    randomIndex = random.randint(0, len(data) - 1)
+    mutation = solution[randomIndex]
+    chance = random.random() <= probability
+    if (chance):
+        if (mutation == 0):
+            solution[randomIndex] = 1
+        else:
+            solution[randomIndex] = 0
+    return solution
 
 
 if __name__ == "__main__":
---
+++ greedy.py
@@ -0,0 +1,50 @@
+#Heuristic algorithm to deal with the Knapsack problem
+
+data = [[4, 2, 0], [5, 2, 1], [7, 11, 2],
+        [7, 11, 3]]  # object set with the properties - [value, weight, id]
+
+container = []  #the container to contain the objects in
+capacity = 10  #weight capacity of the container
+solution = [0] * len(data)  #binary representation of the soultion
+
+ratios = []  #an array to store the ratios of each object
+count = 0  #a count to track the weights of objects before adding them to the container
+
+# For loop to get the ratios of value/weight and add the ratios of each object to the container
+for i in range(len(data)):
+    ratio = data[i][0] / data[i][1]
+    ratios.append([ratio, data[i][2]])
+print(data)
+print(ratios)
+# Sort the resulting ratios in decending order
+decreasing = sorted(ratios, key=lambda x: x[0], reverse=True)
+print(decreasing)
+
+# For loop to add the objects to the container without exceeding the capacity of the container
+for i in range(len(decreasing)):
+    for j in range(len(data)):
+        if (decreasing[i][1] == data[j][2]):
+            if (data[j][1] <= capacity):
+                if (count + data[j][1] <= capacity):
+                    count = count + data[j][1]
+                    container.append(data[j])
+                    index = data[j][2]
+                    solution[index] = 1
+
+print("\nWeights", count)
+print("Here's the resulting container: ", container)
+print("\nBinary string representation of the soultion: ", solution)
+
+CHROMO = [] * 3  # [[binary soultion], fitness, total weight]
+fitness = 0
+totalWeight = 0
+for j in range(len(solution)):
+    if (solution[j] == 1):
+        fitness += data[j][0]
+        totalWeight += data[j][1]
+CHROMO.append(solution)
+CHROMO.append(fitness)
+CHROMO.append(totalWeight)
+print("!!result!!:", CHROMO)
+print("Total value of the soultion:", CHROMO[1],
+      "Total weight of the soultion:", CHROMO[2])
